395c985f72a3dc9168dae562afd986f87532645b
German <german.debustamante@iesnervion.es>
2022-06-02 01:29:00 +0200

    Relacion ternario Player-Formation-Game eliminada

--- InaDraft/data/local/src/main/java/es/iesnervion/gdebustamante/local/room/database/InaDraftDatabase.kt
+++ InaDraft/data/local/src/main/java/es/iesnervion/gdebustamante/local/room/database/InaDraftDatabase.kt
@@ -3,9 +3,6 @@ package es.iesnervion.gdebustamante.local.room.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import es.gdebustamante.inadraft.player.PlayerLocalDataSource
-import es.gdebustamante.inadraft.position.PositionLocalDataSource
-import es.gdebustamante.inadraft.team.TeamLocalDataSource
 import es.iesnervion.gdebustamante.local.room.dao.FormationDao
 import es.iesnervion.gdebustamante.local.room.dao.PlayerDao
 import es.iesnervion.gdebustamante.local.room.dao.PositionDao
@@ -13,8 +10,7 @@ import es.iesnervion.gdebustamante.local.room.dao.TeamDao
 import es.iesnervion.gdebustamante.local.room.dbo.entity.*
 
 @Database(
-    entities = [PlayerDBO::class, TeamDBO::class, PositionDBO::class,
-        PlayerFormationCrossRef::class, FormationDBO::class, GameDBO::class],
+    entities = [PlayerDBO::class, TeamDBO::class, PositionDBO::class, FormationDBO::class, GameDBO::class],
     version = 1
 )
 @TypeConverters(Converters::class)
--- InaDraft/data/local/src/main/java/es/iesnervion/gdebustamante/local/room/dbo/entity/PlayerFormationCrossRef.kt
+++
@@ -1,9 +0,0 @@
-package es.iesnervion.gdebustamante.local.room.dbo.entity
-
-import androidx.room.Entity
-
-@Entity(tableName = "PlayersFormations", primaryKeys = ["playerId", "formationId"])
-data class PlayerFormationCrossRef(
-    val playerId: Int,
-    val formationId: Int
-)
